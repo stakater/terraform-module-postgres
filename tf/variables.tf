cb9841e03ee90b9cb9a5d9c50af86a29ce0b12c6
asim <asimawan.42@gmail.com>
2018-02-07 15:34:36 +0500

    Remove unnecessary variables

--- tf/variables.tf
+++ tf/variables.tf
@@ -30,35 +30,3 @@ variable "database_username" {
 
 variable "database_password" {
 }
-
-variable "backup_retention_period" {
-    default = 14
-}
-
-variable "preferred_backup_window" {
-    default = "02:00-03:00"
-}
-
-variable "preferred_maintenance_window" {
-    default = "wed:03:00-wed:04:00"
-}
-
-variable "cluster_instance_count" {
-    default = "1"
-}
-
-variable "cluster_instance_class" {
-    default = "db.t2.small"
-}
-
-variable "publicly_accessible" {
-    default = false
-}
-
-variable "skip_final_snapshot" {
-    default = true
-}
-
-variable "engine" { }
-
-variable "engine_version" { }
